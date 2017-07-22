a5a2223a48cd8b4d4deae6460d4d5246dcb30dfe
Antoine Albertelli <antoine@antoinealb.net>
2017-07-22 14:41:03 +0800

    Add main.h that was forgotten

---
+++ apps/smart-wellness/src/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "veml6075-uv-sensor/veml6075-uv-sensor.h"
+extern veml6075_dev_t uv_sensor;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
